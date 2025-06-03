a8f5eac309539616bd0dfe4fa98adf4e5d89e10e
Bruno Dias <ibroopi@gmail.com>
2025-06-03 16:48:48 +0100

    Fix mage 2.4.8 compatibility error (#11)

--- Model/Config/Source/LogLevelList.php
+++ Model/Config/Source/LogLevelList.php
@@ -3,14 +3,15 @@
 namespace OuterEdge\OpenTelemetry\Model\Config\Source;
 
 use Magento\Framework\Data\OptionSourceInterface;
-use Monolog\Logger;
+use Monolog\Level;
 
 class LogLevelList implements OptionSourceInterface
 {
     public function toOptionArray()
     {
         $return = [];
-        foreach (Logger::getLevels() as $key => $value) {
+        $levels = array_combine(Level::NAMES, Level::VALUES);
+        foreach ($levels as $key => $value) {
             $return[] = array(
                 'value' => $key,
                 'label' => $key
